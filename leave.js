7f65eb862cdf5f4fe563ce132d5efbd38c680671
Signe Hoel <signe.hoel@gmail.com>
2021-06-19 14:52:29 -0300

    change css

--- leave.js
+++ leave.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
 
 $('#textarea1').val('New Text');
   M.textareaAutoResize($('#textarea1'));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const value = data.get('email');
+    console.log({ value });
+    }
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleSubmit);
