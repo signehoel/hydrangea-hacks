389f75b0e850776052e0ed8ca0f987c9088a7091
Signe Hoel <signe.hoel@gmail.com>
2021-06-19 10:40:36 -0300

    updated html

---
+++ reviews.js
@@ -0,0 +1,10 @@
+fetch('reviews.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
