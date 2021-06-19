699a50c6ef49a0ce98d3f3ada2c148e15f723fb8
Signe Hoel <signe.hoel@gmail.com>
2021-06-19 11:58:02 -0300

    added stars

--- reviews.js
+++ reviews.js
@@ -14,7 +14,20 @@ function appendData(data) {
     for (var i = 0; i < data.length; i++) {
       var div = document.createElement("div");
       div.className = "card-panel deep-purple lighten-5"
-      var html = `<span class=""><h5>${data[i].headline}</h5><p>${data[i].name}</p><p>${data[i].description}</p></span>`
+      var checked = "";
+      var unchecked = "";
+      for (var j = 0; j < data[i].rating; j ++){
+          checked = checked + '<i class="material-icons">star</i>';
+      }
+      for (var j = 0; j < 5 - data[i].rating; j ++) {
+        unchecked = unchecked + '<i class="material-icons">star_border</i>';
+        }
+      var html = `<span class="">
+        <h5>${data[i].headline}</h5>
+        ${checked} ${unchecked}
+        <p>${data[i].name}</p>
+        <p>${data[i].description}</p>
+      </span>`
       div.innerHTML = html;
       mainContainer.appendChild(div);
     }
