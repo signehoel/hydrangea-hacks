0e78b06a6dc1aa67eb986a42aa291c5222e64469
claradawang <dawangc@mcmaster.ca>
2021-06-19 10:36:19 -0400

    colour changes

--- reviews.js
+++ reviews.js
@@ -13,7 +13,7 @@ function appendData(data) {
     var mainContainer = document.getElementById("reviews");
     for (var i = 0; i < data.length; i++) {
       var div = document.createElement("div");
-      div.className = "card-panel"
+      div.className = "card-panel deep-purple lighten-5"
       var html = `<span class=""><h5>${data[i].headline}</h5><p>${data[i].name}</p><p>${data[i].description}</p></span>`
       div.innerHTML = html;
       mainContainer.appendChild(div);
