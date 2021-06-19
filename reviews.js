7a39f74ea9b5fe5d61c864621987ae3a8404a17e
Signe Hoel <signe.hoel@gmail.com>
2021-06-19 11:18:21 -0300

    updated styling

--- reviews.js
+++ reviews.js
@@ -7,4 +7,15 @@ fetch('reviews.json')
   })
   .catch(function (err) {
     console.log(err);
-  });
+});
+
+function appendData(data) {
+    var mainContainer = document.getElementById("reviews");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.className = "card-panel"
+      var html = `<span class=""><h5>${data[i].headline}</h5><p>${data[i].name}</p><p>${data[i].description}</p></span>`
+      div.innerHTML = html;
+      mainContainer.appendChild(div);
+    }
+  }
