b6dff669b5b18e374411eb05cb2d5b664494d1bd
Signe Hoel <signe.hoel@gmail.com>
2021-06-19 12:29:00 -0300

    changed accessibility

--- reviews.js
+++ reviews.js
@@ -12,23 +12,39 @@ fetch('reviews.json')
 function appendData(data) {
     var mainContainer = document.getElementById("reviews");
     for (var i = 0; i < data.length; i++) {
-      var div = document.createElement("div");
-      div.className = "card-panel deep-purple lighten-5"
-      var checked = "";
-      var unchecked = "";
-      for (var j = 0; j < data[i].rating; j ++){
-          checked = checked + '<i class="material-icons">star</i>';
-      }
-      for (var j = 0; j < 5 - data[i].rating; j ++) {
-        unchecked = unchecked + '<i class="material-icons">star_border</i>';
-        }
-      var html = `<span class="">
-        <h5>${data[i].headline}</h5>
-        ${checked} ${unchecked}
-        <p>${data[i].name}</p>
-        <p>${data[i].description}</p>
-      </span>`
-      div.innerHTML = html;
-      mainContainer.appendChild(div);
+        console.log(data[i].website);
+        let link = data[i].website;
+        let name = data[i].name;
+        let rating = data[i].rating;
+        let headline = data[i].headline;
+        let description = data[i].description;
+
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            let url = tabs[0].url;
+            if (link === url){
+                var div = document.createElement("div");
+    
+                div.className = "card-panel deep-purple lighten-5";
+                var checked = "";
+                var unchecked = "";
+                for (var j = 0; j < rating; j ++){
+                    checked = checked + '<i class="material-icons">star</i>';
+                }
+                for (var j = 0; j < 5 - rating; j ++) {
+                    unchecked = unchecked + '<i class="material-icons">star_border</i>';
+                }
+                var html = `<span class="">
+                    <h5>${headline}</h5>
+                    ${checked} ${unchecked}
+                    <p>${name}</p>
+                    <p>${description}</p>
+                </span>`
+                div.innerHTML = html;
+                mainContainer.appendChild(div);
+            }  
+        });
+        // use `url` here inside the callback because it's asynchronous!
+        
+         
     }
   }
